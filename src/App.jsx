be9ae2ff582ab317551853a4240eb9aafb1ad59b
Yurii Kramar <y.kramar15@gmail.com>
2024-11-23 18:41:20 +0100

    solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,52 @@
 /* eslint-disable max-len */
 
+import { useState } from 'react';
 import './App.scss';
 import moviesFromServer from './api/movies.json';
 import { MovieList } from './components/MovieList/MovieList';
 
-export const App = () => (
-  <div className="page">
-    <div className="page-content">
-      <MovieList movies={moviesFromServer} />
-    </div>
+const getPrepearMovies = (movies, query) => {
+  const cloneMovies = [...movies];
+  const normalizeStr = query.toLowerCase().trim();
 
-    <div className="sidebar" data-cy="Sidebar">
-      Sidebar will be here
+  if (normalizeStr) {
+    return cloneMovies.filter(movie => {
+      if (movie.title.toLowerCase().trim().includes(normalizeStr)) {
+        return movie;
+      }
+
+      if (movie.description.toLowerCase().trim().includes(normalizeStr)) {
+        return movie;
+      }
+
+      return '';
+    });
+  }
+
+  return cloneMovies;
+};
+
+export const App = () => {
+  const [query, setQuery] = useState('');
+  const visibleMovies = getPrepearMovies(moviesFromServer, query);
+
+  // function handelImput (e) {
+  //   return e.target
+  // }
+  return (
+    <div className="page">
+      <div className="page-content">
+        <MovieList movies={visibleMovies} />
+      </div>
+
+      <div className="sidebar" data-cy="Sidebar">
+        <input
+          type="text"
+          value={query}
+          onChange={e => setQuery(e.target.value)}
+        />
+        Sidebar will be here
+      </div>
     </div>
-  </div>
-);
+  );
+};
